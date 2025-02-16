c5532428845b7e24fd2f41d6f07e559f3dd3b989
sergiofdf <sergiofdf@gmail.com>
2025-02-16 10:34:21 -0300

    fix: swagger contactapi

--- ContactApi/Program.cs
+++ ContactApi/Program.cs
@@ -54,12 +54,8 @@ builder.Services.AddSystemMetrics();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpMetrics();
 
